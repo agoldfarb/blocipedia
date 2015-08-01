3aa82205bfebae04d4cb9c5e7e8540577868c62e
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-08-01 17:00:02 -0500

    added bootstrap

--- Gemfile
+++ Gemfile
@@ -52,4 +52,5 @@ group :development, :test do
 end
 
 gem 'devise'
+gem 'bootstrap-sass'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= requite bootstrap
 //= require_tree .
