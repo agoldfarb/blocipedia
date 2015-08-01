bc061c159c72264b3cb08e08711f7715b9dbc8cd
Allison Goldfarb <allisonrgoldfarb@gmail.com>
2015-08-01 16:53:51 -0500

    user sign up

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,6 @@
 ActiveRecord::Schema.define(version: 20150731215444) do
 
   create_table "users", force: :cascade do |t|
-    t.string   "name"
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -25,6 +24,13 @@ ActiveRecord::Schema.define(version: 20150731215444) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
+    t.integer  "failed_attempts",        default: 0,  null: false
+    t.string   "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
   end
